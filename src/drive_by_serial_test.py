7e7dc2a720bbf3545211ffa9ca1723b5a3484c77
Henrik Lurz <henrik.lurz@web.de>
2025-03-13 15:02:18 +0100

    Working serial connection

--- src/drive_by_serial_test.py
+++ src/drive_by_serial_test.py
@@ -1,13 +1,18 @@
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+ser = serial.Serial("/dev/ttyUSB0")  # open serial port
 print(ser.name)
 
-chain_velocity: int = 170
+chain_velocity: int = 200
+
+heart_beat: bool = True
 
 command_left: str = ":ML=" + str(chain_velocity) + "!"
 command_right: str = ":MR=" + str(chain_velocity) + "!"
 
-ser.write(command_left)
-ser.write(command_right)
-
+while(1):
+    command_heart_beat: str = ":WD=" + str(int(heart_beat)) + "!"
+    ser.write(command_heart_beat.encode())
+    ser.write(command_left.encode())
+    ser.write(command_right.encode())
+    heart_beat = not heart_beat
