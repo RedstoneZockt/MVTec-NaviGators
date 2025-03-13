81311ba7e4607d115af21ae539fd7a21d928a5e8
Maciej Sadowski <maciej.sadowski.pwrrt@gmail.com>
2025-03-13 15:30:13 +0100

    Keyboard controller ready to run

--- controoller.py
+++ controoller.py
@@ -2,6 +2,7 @@ import socket
 #import serial
 import time
 
+from can.interfaces import serial
 
 
 # MODE:
@@ -60,6 +61,12 @@ class Controller():
         # Speed
         self.speed = 0
 
+        #serial
+        self.serial = SerialCommunication("/dev/ttyUSB0")
+
+        self.right_speed = 0
+        self.left_speed = 0
+
     def set_mode(self, mode):
         self.mode = mode
         print("Mode changed to ", mode)
@@ -73,16 +80,16 @@ class Controller():
         self.set_left_side(self.speed)
 
     def go_back(self):
-        # TODO: code to go back
-        print("Going backward")
+        self.set_right_side(-self.speed)
+        self.set_left_side(-self.speed)
 
     def go_right(self):
-        # TODO: code to go right
-        print("Going right")
+        self.set_right_side(self.speed)
+        self.set_left_side(-self.speed)
 
     def go_left(self):
-        # TODO: code to go left
-        print("Going left")
+        self.set_right_side(-self.speed)
+        self.set_left_side(self.speed)
 
     def go_stop(self):
         self.set_right_side(0.0)
@@ -91,11 +98,11 @@ class Controller():
 
     # Right side control
     def set_right_side(self, speed):
-        print("Setting right side to ", self.speed_normalize_funcion(speed))
+        self.left_speed = self.speed_normalize_funcion(speed)
 
     # Left side control
     def set_left_side(self, speed):
-        print("Setting left side to ", self.speed_normalize_funcion(speed))
+        self.right_speed = self.speed_normalize_funcion(speed)
 
     def speed_normalize_funcion(self, speed):
         in_min = -100
@@ -104,15 +111,28 @@ class Controller():
         out_max = 512
         return (speed - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
+    def controller_data_sender(self):
+        self.serial.send_data(":ML=" + str(self.left_speed) + "!")
+        self.serial.send_data(":MR=" + str(self.right_speed) + "!")
 
 
+class SerialCommunication():
+    def __init__(self, port):
+        self.serial = serial.Serial(port)
+        self.heart_beat = True
+
+    def send_data(self, data):
+        command_heart_beat: str = ":WD=" + str(int(self.heart_beat)) + "!"
+        self.serial.write(command_heart_beat.encode())
+        serial.write(data.encode())
+        self.heart_beat = not self.heart_beat
 
 communication = Communication("127.0.0.1", 6000)
 
 while True:
     data = communication.listen()
-    print(data)
     communication.data_recognition_controller(data)
+    communication.controller.controller_data_sender()
 
 
 
