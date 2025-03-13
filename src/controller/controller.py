f1c3bdc3767f3fda81d7c0d8993b0d1c8677d110
Maciej Sadowski <maciej.sadowski.pwrrt@gmail.com>
2025-03-13 18:55:05 +0100

    Controller ready for odom test

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,5 +1,5 @@
 import socket
-#import serial
+import serial
 import time
 import threading
 import comunication
@@ -8,7 +8,7 @@ import odometry
 serial_port = "/dev/ttyUSB0"
 server_address = "127.0.0.1"
 server_port = 5000
-debug = True
+debug = False
 # MODE:
 # 1 - Forward, Backward, Turn left, Turn right control
 # 2 - To goal controller
